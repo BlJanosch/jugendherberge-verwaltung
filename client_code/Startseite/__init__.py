118683290bcc76e8fb4586c447f12223d4a17f5c
jannik-eduard.laengle@student.htl-rankweil.at <jannik-eduard.laengle@student.htl-rankweil.at>
2024-10-28 10:24:35 +0000

    Created Design ohne Titel(1).png, Design ohne Titel.png, Gebucht and Untitled design(1).png; Edited Startseite and Database_Server

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -14,7 +14,9 @@ class Startseite(StartseiteTemplate):
     # Any code you write here will run before the form opens.
     data = anvil.server.call("get_jugendherbergen", "name, JID")
     self.drop_down_1.items = data
-
+    print(anvil.server.call("getdata1"))
+    print("Data2")
+    print(anvil.server.call("getdata2"))
     self.LoadZimmerData()
     self.LoadGaeste()
     
@@ -40,6 +42,32 @@ class Startseite(StartseiteTemplate):
     self.check_box_1.text = f"{gäste[0][2]} {gäste[0][3]}"
     self.check_box_2.text = f"{gäste[1][2]} {gäste[1][3]}"
     self.check_box_3.text = f"{gäste[2][2]} {gäste[2][3]}"
-  
-  
+
+  def button_1_click(self, **event_args):
+    # Befüllen der Tabelle bucht mit den Daten
+    ZimmerNummer = self.drop_down_2.selected_value
+    ZID = anvil.server.call("get_zimmerid", ZimmerNummer)
+    OldDatum = str(self.date_picker_1.date)
+    Datum = OldDatum.replace("-", ".")
+    print(Datum)
+    anvil.server.call("set_bucht_for_jugendherberge", 10, ZID, Datum)
+    anvil.server.call('update_zimmer', ZID)
+    # Befüllen der Tabelle buchtmit mit den Daten
+    BID = anvil.server.call("get_bid", ZID, Datum)
+    Begleiter = []
+    if (self.check_box_1.checked):
+      vorname, nachname = self.check_box_1.text.split(" ")
+      Begleiter.append(anvil.server.call("get_gid", vorname, nachname))
+    if (self.check_box_2.checked):
+      vorname, nachname = self.check_box_2.text.split(" ")
+      Begleiter.append(anvil.server.call("get_gid", vorname, nachname))
+    if (self.check_box_3.checked):
+      vorname, nachname = self.check_box_3.text.split(" ")
+      Begleiter.append(anvil.server.call("get_gid", vorname, nachname))
+    print(Begleiter)
+    for x in Begleiter:
+      anvil.server.call("set_buchmit_for_jugendherberge", BID, x)
+
+    open_form('Gebucht')
+
     
--- server_code/Database_Server.py
+++ server_code/Database_Server.py
@@ -38,6 +38,68 @@ def get_gast_for_jugendherberge(rows="*"):
     conn.close()
     return res
 
+@anvil.server.callable
+def set_buchmit_for_jugendherberge(bid, gid):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO buchtmit (BID, GID) VALUES ({bid}, {gid})")
+    conn.commit()
+    conn.close()
 
+@anvil.server.callable
+def set_bucht_for_jugendherberge(gid, zid, datum):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO bucht (GID, ZID, Datum) VALUES ({gid}, {zid}, '{datum}')")
+    conn.commit()
+    conn.close()
+
+@anvil.server.callable
+def get_zimmerid(zimmernummer):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    res = cursor.execute(f"SELECT ZID FROM zimmer WHERE zimmernummer={zimmernummer}").fetchone()
+    conn.close()
+    return res[0]
+
+@anvil.server.callable
+def get_bid(zid, datum):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    res = cursor.execute(f"SELECT BID FROM bucht WHERE ZID={zid} AND Datum='{datum}'").fetchone()
+    conn.close()
+    return res[0]
+
+@anvil.server.callable
+def get_gid(vorname, nachname):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    res = cursor.execute(f"SELECT GID FROM gast WHERE vorname='{vorname}' AND nachname='{nachname}'").fetchone()
+    conn.close()
+    return res[0]
+
+@anvil.server.callable
+def update_zimmer(zid):
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE zimmer SET gebucht=1 WHERE ZID={zid}")
+    conn.commit()
+    conn.close()
+
+@anvil.server.callable
+def getdata1():
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    res = list(cursor.execute("select * from bucht"))
+    conn.close()
+    return res
+
+@anvil.server.callable
+def getdata2():
+    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+    cursor = conn.cursor()
+    res = list(cursor.execute("select * from buchtmit"))
+    conn.close()
+    return res
 
 
